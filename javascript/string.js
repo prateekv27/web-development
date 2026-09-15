7418cf91bfd23d7dad1fff8abf04d2676c0b262a
prateekv27 <prateekv887@gmail.com>
2026-09-16 03:19:08 +0530

    js today

--- javascript/string.js
+++ javascript/string.js
@@ -1,11 +1,35 @@
-let b1 = `prateek`
-let b2 = `ritik`
-let friend = `${b2} is a friend of ${b1}`
-console.log(friend)
+// let b1 = `prateek`
+// let b2 = `ritik`
+// let friend = `${b2} is a friend of ${b1}`
+// console.log(friend)
 
 
-//escaping character
+// //escaping character
 
 
-let fruit = `bana\`na`
-console.log(fruit)
+// // let fruit = `bana\`na`
+// // console.log(fruit)
+
+
+// let fruit = `bana\tna`
+// let fruit2 = `bana\nna`
+// let fruit3 = `bana\rna`
+// console.log(fruit);
+// console.log(fruit2);
+
+// console.log(fruit3);
+
+
+// let str = "Prateek";
+
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+// }
+
+let str = "har\""
+console.log(str.length)
+
+let str2 = "give me 1000"
+let amt = parseInt(str2.slice(8));
+console.log(amt);
+console.log(typeof amt);
